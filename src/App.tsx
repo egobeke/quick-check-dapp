1eaff0d80ca4d163e78cfe067c43c022851080e9
egobeke <aranonupurity@gmail.com>
2025-04-10 18:09:49 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Quick Check Dapp</h1>
+      <p>Welcome to the future of trust!</p>
+    </div>
+  );
+}
+
+export default App;
